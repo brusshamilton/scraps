de908eac0090070df73d5b30f6a1727c3a72d043
Russ Hamilton <behamilton@google.com>
2023-03-31 15:22:38 +0000

    Fix a few minor issues

--- fledge/biddingLogic.js
+++ fledge/biddingLogic.js
@@ -1,7 +1,7 @@
 function generateBid(
     interestGroup, auctionSignals, perBuyerSignals, trustedBiddingSignals,
     browserSignals, directFromSellerSignals) {
-  return {bid: 1, render: 'https://brusshamilton.github.io/fledge/fake_ad.html'};
+  return {bid: 1, render: 'https://brusshamilton.github.io/scraps/fledge/fake_ad.html'};
 }
 
 function reportWin(auctionSignals, perBuyerSignals, sellerSignals,
